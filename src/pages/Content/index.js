5a7e3d507d96eacdfdfdb1fa530782901354461e
charan r <charan@coditation.com>
2023-03-01 15:51:22 +0530

    added Svg element

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -11,7 +11,35 @@ if (url) {
   const modalContent = document.createElement('div');
   modalContent.classList.add('modal-content');
   const closeButton = document.createElement('span');
-  closeButton.innerText = `x`;
+
+  // Adding Svg Element
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '24');
+  svg.setAttribute('height', '24');
+  svg.setAttribute('viewBox', '0 0 24 24');
+  svg.setAttribute('fill', 'none');
+
+  const path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path1.setAttribute('d', 'M18 6L6 18');
+  path1.setAttribute('stroke', 'black');
+  path1.setAttribute('stroke-width', '2');
+  path1.setAttribute('stroke-linecap', 'round');
+  path1.setAttribute('stroke-linejoin', 'round');
+
+  const path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path2.setAttribute('d', 'M6 6L18 18');
+  path2.setAttribute('stroke', 'black');
+  path2.setAttribute('stroke-width', '2');
+  path2.setAttribute('stroke-linecap', 'round');
+  path2.setAttribute('stroke-linejoin', 'round');
+
+  svg.appendChild(path1);
+  svg.appendChild(path2);
+
+  //
+
+  // closeButton.innerText = `x`;
+  closeButton.appendChild(svg);
   closeButton.classList.add('close');
   const iframe = document.createElement('iframe');
   iframe.width = '582px';
@@ -24,7 +52,7 @@ if (url) {
   const div = document.createElement('div');
   div.classList.add('button-container');
   const button = document.createElement('div');
-  div.classList.add('chat-button');
+  button.classList.add('chat-button');
   button.addEventListener('click', () => {
     modal.style.display = 'block';
   });
